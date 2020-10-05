27bccbe345b3003a29603f8c7db945e8bb19f920
Andrea Osmar <andrea88@live.se>
2020-10-05 16:05:00 +0200

    unit tests complete

--- src/7-is-empty.js
+++ src/7-is-empty.js
@@ -1,9 +1,12 @@
 export const isEmpty = (stringArrayOrObject) => {
   const type = typeof stringArrayOrObject
 
-  if (type === 'string') {
-    return stringArrayOrObject === ''
+  if (type === 'string' && stringArrayOrObject === '') {
+    return true;
+  } else if (Array.isArray(stringArrayOrObject) && stringArrayOrObject.length === 0) {
+    return true;
+  } else if (type === 'object' && Object.keys(stringArrayOrObject).length === 0) {
+    return true;
   }
-
   return false
 }
--- src/8-hacker-speak.js
+++ src/8-hacker-speak.js
@@ -1,3 +1,15 @@
 export const hackerSpeak = (words) => {
+  words = words.replace(/a/gi, '4')
+  words = words.replace(/e/gi, '3')
+  words = words.replace(/i/gi, '1')
+  words = words.replace(/o/gi, '0')
+  words = words.replace(/s/gi, '5')
+  console.log(words)
   return words
 }
+
+// replace method, regular expression
+//npm run test -- -t 8-hacker-speak
+// gi implies that we are looking thru the whola array and both uppercas & lowercase
+// g = perfrom global emplacement
+// gi = case insensitive (looking for all)
